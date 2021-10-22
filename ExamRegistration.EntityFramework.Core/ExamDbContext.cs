85d81b1edaef33f96a27c945f248e443d6b61d57
Javidan <j.aliyev@tayqatech.com>
2021-10-22 16:02:57 +0400

    init commit

---
+++ ExamRegistration.EntityFramework.Core/ExamDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRegistration.EntityFramework.Core
+{
+    public class ExamDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration; 
+        public ExamDbContext(DbContextOptions<ExamDbContext> options, IConfiguration configuration):base(options)
+        {
+            _configuration = configuration; 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration["ConnectionStrings"]);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+    }
+}
